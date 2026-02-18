e5849e63cfc9810a713f5b6157f197a5f7851b84
emreerguven <emrerguven@gmail.com>
2026-02-18 22:10:36 +0300

    Duration change main.py

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def optimize(request: OptimizationRequest):
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
     search_parameters.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
-    search_parameters.time_limit.seconds = 30 # Derin arama için süre
+    search_parameters.time_limit.seconds = 5 # Derin arama için süre
 
     solution = routing.SolveWithParameters(search_parameters)
 
